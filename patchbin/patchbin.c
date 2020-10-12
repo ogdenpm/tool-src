d73fba8421b961c44b8b0b079dc3a3b349ae6f5e
Mark <mark.pm.ogden@btinternet.com>
2020-10-12 11:58:22 +0100

    Added copyright notice under MIT License (MIT) to source files

--- patchbin/patchbin.c
+++ patchbin/patchbin.c
@@ -1,3 +1,29 @@
+/****************************************************************************
+ *  patchbin: apply patches to a CPM .com file                              *
+ *  Copyright (C) 2020 Mark Ogden <mark.pm.ogden@btinternet.com>            *
+ *                                                                          *
+ *  Permission is hereby granted, free of charge, to any person             *
+ *  obtaining a copy of this software and associated documentation          *
+ *  files (the "Software"), to deal in the Software without restriction,    *
+ *  including without limitation the rights to use, copy, modify, merge,    *
+ *  publish, distribute, sublicense, and/or sell copies of the Software,    *
+ *  and to permit persons to whom the Software is furnished to do so,       *
+ *  subject to the following conditions:                                    *
+ *                                                                          *
+ *  The above copyright notice and this permission notice shall be          *
+ *  included in all copies or substantial portions of the Software.         *
+ *                                                                          *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,         *
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF      *
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  *
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    *
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    *
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       *
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  *
+ *                                                                          *
+ ****************************************************************************/
+
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -10,61 +36,61 @@ char *invokedBy;
 
 char *gethex(char *s, unsigned *val)
 {
-	*val = 0;
-	while (*s == ' ' || *s == '\t')
-		s++;
-	if (!isxdigit(*s))
-		return 0;
-	while (isxdigit(*s)) {
-		*val = *val * 16 + (isdigit(*s) ? *s - '0' : toupper(*s) - 'A' + 10);
-		s++;
-	}
-	return s;
+    *val = 0;
+    while (*s == ' ' || *s == '\t')
+        s++;
+    if (!isxdigit(*s))
+        return 0;
+    while (isxdigit(*s)) {
+        *val = *val * 16 + (isdigit(*s) ? *s - '0' : toupper(*s) - 'A' + 10);
+        s++;
+    }
+    return s;
 }
 
 
 __declspec(noreturn) void usage(char *fmt, ...) {
 
-	if (fmt) {
-		va_list args;
-		va_start(args, fmt);
-		vfprintf(stderr, fmt, args);
-		va_end(args);
-	}
-	fprintf(stderr, "\nUsage: %s -v | -V | patchfile filetopatch\n", invokedBy);
+    if (fmt) {
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+    }
+    fprintf(stderr, "\nUsage: %s -v | -V | patchfile filetopatch\n", invokedBy);
 
-	exit(1);
+    exit(1);
 }
 
 int main(int argc, char **argv)
 {
-	FILE *pfp, *fp;
-	unsigned addr, patch;
-	char line[256];
-	char *s;
+    FILE *pfp, *fp;
+    unsigned addr, patch;
+    char line[256];
+    char *s;
     long fsize;
     int append;
-	invokedBy = argv[0];
+    invokedBy = argv[0];
 
-	if (argc == 2 && _stricmp(argv[1], "-v") == 0) {
-		showVersion(stdout, argv[1][1] == 'V');
-		exit(0);
-	}
-	if (argc != 3 || (pfp = fopen(argv[1], "rt")) == NULL || (fp = fopen(argv[2], "r+b")) == NULL)
-		usage(NULL);
+    if (argc == 2 && _stricmp(argv[1], "-v") == 0) {
+        showVersion(stdout, argv[1][1] == 'V');
+        exit(0);
+    }
+    if (argc != 3 || (pfp = fopen(argv[1], "rt")) == NULL || (fp = fopen(argv[2], "r+b")) == NULL)
+        usage(NULL);
 
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
 
 
-	while (fgets(line, 256, pfp)) {
-		for (s = line; *s && (*s == ' ' || *s == '\t' || *s == '\n'); s++)
-			;
+    while (fgets(line, 256, pfp)) {
+        for (s = line; *s && (*s == ' ' || *s == '\t' || *s == '\n'); s++)
+            ;
         if (!*s || *s == '#')
-			continue;
+            continue;
 
-		if ((s = gethex(s, &addr)) == NULL || addr < 0x100) 
-			fprintf(stderr, "bad address: %s", line);
+        if ((s = gethex(s, &addr)) == NULL || addr < 0x100) 
+            fprintf(stderr, "bad address: %s", line);
         else {
             if ((long)addr - 0x100 >= fsize) {
                 fseek(fp, 0, SEEK_END);
@@ -91,8 +117,8 @@ int main(int argc, char **argv)
                 }
             }
         }
-	}
-	fclose(pfp);
-	fclose(fp);
-	return 0;
+    }
+    fclose(pfp);
+    fclose(fp);
+    return 0;
 }
