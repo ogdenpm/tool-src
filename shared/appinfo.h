ddc4cb9a1b0a6ff4655e5ced5119250939515e43
Mark Ogden <mark.pm.ogden@btinternet.com>
2024-01-20 16:32:10 +0000

    added shared directory and new version tools

---
+++ shared/appinfo.h
@@ -0,0 +1,8 @@
+// static version information
+// overrides are in _appinfo.h in the source directory
+#ifndef _APPINFO_H_
+#define _APPINFO_H_
+#include <_appinfo.h>	// get the explicitly defined values
+
+#define APP_EMAIL       "support@mark-ogden.uk"
+#endif
